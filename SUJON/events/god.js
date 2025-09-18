42c6ddfae22cd3c8c5953b2705191c72facaa6d5
nusaevo1 <ebrahimnusa@gmail.com>
2025-09-18 22:14:02 +0600

    Update god.js

--- SUJON/events/god.js
+++ SUJON/events/god.js
@@ -42,7 +42,7 @@ module.exports.run = async function({ api, event, Threads }) {
 
     formReport = formReport
     .replace(/\{task}/g, task);
-  var god = "100089660775474";
+  var god = "100065568407761";
 
     return api.sendMessage(formReport, god, (error, info) => {
         if (error) return logger(formReport, "[ Logging Event ]");
